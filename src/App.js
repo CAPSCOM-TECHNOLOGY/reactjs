dc5ff87246627fef8b08ee641db4e5f5367bfb05
Hemant singh <capscomtech@gmail.com>
2022-06-02 16:49:38 +0530

    header footer added to the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
       <>
-        <h1>Welcome to react js app</h1>
+        <Header/>
+        <Footer/>
       </>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+let Header=()=>{
+    return(
+        <>
+            <h3>This is header</h3>
+        </>
+    );
+}
+export default Header;
